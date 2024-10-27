b846c679924d3108f504a488b24cb16eb6cc2f1f
Sean Enck <sean@ttypty.com>
2024-10-27 09:29:05 -0400

    formatting

--- internal/processing/core.go
+++ internal/processing/core.go
@@ -13,8 +13,9 @@ import (
 type (
 	// Configuration is the overall configuration
 	Configuration struct {
-		handler  *processHandler
-		context  cli.Settings
+		handler *processHandler
+		context cli.Settings
+
 		Indexing struct {
 			Enabled bool `yaml:"enabled"`
 			Strict  bool `yaml:"strict"`
